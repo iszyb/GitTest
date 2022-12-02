7ff75e24be3917b1f1774f75ffe73cd7360fe50f
Zyb <Zyb@guigu.com>
2022-12-02 16:02:48 +0800

    hot commit2

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello");
 
     }
 }
