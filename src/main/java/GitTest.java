a90b8eccad766406a64b156f8af01dab2877e893
Zyb <Zyb@guigu.com>
2022-12-02 15:41:43 +0800

    first commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * @author W10
+ * @version 1.0
+ * Create by 2022/12/2 15:39
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello");
+
+    }
+}
